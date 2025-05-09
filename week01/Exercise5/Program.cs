864da0652839876b10326f7dd5c44cebef548a37
gift-ndoma <giftndoma87@gmail.com>
2025-05-09 19:45:06 +0100

    finalized week one exercises

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -11,30 +11,36 @@ class Program
         int squareNum = SquareNumber(num);
 
         DisplayResult(userName, squareNum);
-        
-    static void DisplayWelcome() {
+    }
+
+    static void DisplayWelcome() 
+    {
         Console.WriteLine("Welcome to the Program!");
     }
 
-    static string PromptUserName() {
+    static string PromptUserName() 
+    {
         Console.Write("Please enter your name: ");
         string name = Console.ReadLine();
         return name;
     }
 
-    static int PromptUserNumber() {
+    static int PromptUserNumber() 
+    {
         Console.Write("Please enter your favorite number: ");
         string numberInput = Console.ReadLine();
         int number = int.Parse(numberInput);
         return number;
     }
     
-    static int SquareNumber(int number) {
+    static int SquareNumber(int number) 
+    {
         int numSquare = number * number;
         return numSquare;
     }
 
-    static void DisplayResult(string name, int number) {
+    static void DisplayResult(string name, int number) 
+    {
         Console.WriteLine($"{name}, the square of your number is {number}");
     }
 }
