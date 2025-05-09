a948bedee44c403400b6077ec158409f8a3d8316
gift-ndoma <giftndoma87@gmail.com>
2025-05-09 15:43:41 +0100

    completed exercise 4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,44 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        int number = -1;
+        int total = 0;
+        int highestNum = -1;
+        int lowestNum = 9999999;
+        List<int> numberList = new List<int>();
+
+        while (number != 0) {
+            Console.Write("Enter your number: ");
+            string numberInput = Console.ReadLine();
+            number = int.Parse(numberInput);
+            if (number != 0) {
+
+                numberList.Add(number);
+                total += number;
+                
+                if (number > highestNum) {
+                    highestNum = number;
+                }
+
+                if (number < lowestNum && number > 0) {
+                    lowestNum = number;
+                }
+            }
+        }
+        Console.WriteLine($"The sum is: {total}");
+        double average = (double)total / numberList.Count;
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest Number is: {highestNum}");
+        Console.WriteLine($"The largest Number is: {lowestNum}");
+
+        numberList.Sort();
+        Console.WriteLine("The sorted list is: ");
+        foreach (int num in numberList) {
+            Console.WriteLine(num);
+        }
     }
 }
