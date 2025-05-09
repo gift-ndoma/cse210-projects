aab9f0b25dfedf064d26eb852c5a93da8a8dba0a
gift-ndoma <giftndoma87@gmail.com>
2025-05-09 11:26:38 +0100

    initial commit

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        // Ask the user for the magic number
+        Console.Write("Enter the Magic Number: ");
+        string magicInput = Console.ReadLine();
+        float magicNumber = int.Parse(magicInput);
+
+        //Asks the user for guess
+        Console.Write("Enter your guess: ");
+        string guessInput = Console.ReadLine();
+        float guess = int.Parse(guessInput);
+
+        if (guess < magicNumber) {
+            Console.WriteLine("Higher");
+        }
+        else if (guess > magicNumber) {
+            Console.WriteLine("Lower!");
+        }
+        else {
+            Console.WriteLine("You guessed it!");
+        }
     }
 }
