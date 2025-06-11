82fc668c61653fe5ecae61ebada092449d2781b1
gift-ndoma <giftndoma87@gmail.com>
2025-06-11 17:04:51 +0100

    initial commit

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(new Square("Blue", 12));
+        shapes.Add(new Circle("Red", 10));
+        shapes.Add(new Rectangle("Orange", 10, 12));
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine(shape.GetArea ());
+        }
     }
 }
